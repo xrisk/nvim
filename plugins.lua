f0786f5ea1ea4f7160e4c989f18b3d3ee081b13a
Pham Huy Hoang <hoangtun0810@gmail.com>
2023-05-03 20:01:52 +0900

    notify of plugins lazy loaded by default

--- plugins.lua
+++ plugins.lua
@@ -53,6 +53,13 @@ local plugins = {
   --   enabled = false
   -- },
 
+  -- All NvChad plugins are lazy-loaded by default
+  -- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
+  -- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
+  -- {
+  --   "mg979/vim-visual-multi",
+  --   lazy = false,
+  -- }
 }
 
 return plugins
