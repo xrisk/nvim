4e0ccfe72c82a6ed9dd9bd56962b70aed8be9120
siduck <siduck@tutanota.com>
2024-01-08 06:16:29 +0530

    replace null-ls with conform

--- mappings.lua
+++ mappings.lua
@@ -4,6 +4,15 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
+
+    --  format with conform
+    ["<leader>fm"] = {
+      function()
+        require("conform").format()
+      end,
+      "formatting",
+    }
+
   },
   v = {
     [">"] = { ">gv", "indent"},
